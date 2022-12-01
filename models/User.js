940b5485d297f4c50c28a957d76cec7a1b5000fa
Darby Biddy <darbybiddy3@gmail.com>
2022-12-01 16:31:25 -0600

    added password verifcations and sercurity

--- models/User.js
+++ models/User.js
@@ -1,52 +1,77 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connections");
+const bcrypt = require("bcrypt");
 
 //create a User Model
-class User extends Model {}
+class User extends Model {
+  //set up method to run on instance data (per user) to check password
+  checkPassword(loginPw){
+    return bcrypt.compareSync(loginPw, this.password)
+  }
+}
 
 //define table columns and configurations
 User.init(
   {
     //define an id column
-    id:{
-        //use the special sequelize datatypes object to provide what type of data it is
-    type: DataTypes.INTEGER,
-    //this is equivilent of SQL's `NOT NULL` option
-    allowNull: false,
-    //instruct that this is the primary key
-    primaryKey: true,
-    //turn on auto increment
-    autoIncrement: true
+    id: {
+      //use the special sequelize datatypes object to provide what type of data it is
+      type: DataTypes.INTEGER,
+      //this is equivilent of SQL's `NOT NULL` option
+      allowNull: false,
+      //instruct that this is the primary key
+      primaryKey: true,
+      //turn on auto increment
+      autoIncrement: true,
     },
     //define a username column
-    username:{
-        type: DataTypes.STRING,
-        allowNull: false
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     //define an email column
-    email:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        // there cannot be any duplicate email values in this table
-        unique: true,
-        //if allownull is set to false, we can run our data through validators before creating the tables data
-        validate: {
-            isEmail: true
-        }
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // there cannot be any duplicate email values in this table
+      unique: true,
+      //if allownull is set to false, we can run our data through validators before creating the tables data
+      validate: {
+        isEmail: true,
+      },
     },
     //define a password column
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            //this means the password must be at least four characters long
-            len: [4]
-        }
-    }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        //this means the password must be at least four characters long
+        len: [4],
+      },
+    },
   },
 
   {
     //TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+    hooks: {
+      //set up beforeCreate lifecycle "hook" functionlity
+      // We use the beforeCreate() hook to execute the bcrypt hash function on the plaintext password.
+      //In the bcrypt hash function, we pass in the userData object that contains the
+      //plaintext password in the password property.
+      //We also pass in a saltRound value of 10.
+      //The resulting hashed password is then passed to the Promise object as a newUserData
+      // object with a hashed password property. The return statement then exits out of the function,
+      //returning the hashed password in the newUserData function.
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password,10);
+        return updatedUserData;
+      },
+
+    },
 
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
@@ -61,4 +86,4 @@ User.init(
   }
 );
 
-module.exports = User
+module.exports = User;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,7 +6,7 @@ const { User } = require("../../models");
 router.get("/", (req, res) => {
   //access our User model and run .findAll()method)
   User.findAll({
-    attributes: {exclude: ['password']}
+    attributes: { exclude: ["password"] },
   })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
@@ -16,83 +16,109 @@ router.get("/", (req, res) => {
 });
 
 //Get/api/users/1
-  router.get("/:id", (req, res) => {
-    User.findOne({
-        attributes: {exclude: ['password']},
-      where: {
-        id: req.params.id,
-      },
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "no user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
     })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: "no user found with this id" });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // POST /api/users
-router.post('/', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-    User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+router.post("/", (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//verify the users identity
+router.post("/login", (req, res) => {
+  // expects {email :'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+      where: {
+        email: req.body.email,
+      }
+    }).then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(400).json({ message: "no user with that email address!" });
+        return;
+      }  
+      
+      //verify user
+      const validPassword = dbUserData.checkPassword(req.body.password)
+      if(!validPassword){
+        res.status(400).json({message: 'incorrect password'})
+        return
+      }
+
+      res.json({ user: dbUserData, message:'you are now logged in' });
+    
+    });
+});
 
 //Put/api/user/1
 router.put("/:id", (req, res) => {
-        //expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  //expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
-        // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
-        User.update(req.body,{
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(dbUserData => {
-            if(!dbUserData[0]){
-                res.status(404).json({message: 'no user found with this id'})
-                return
-            }
-            res.json(dbUserData)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
+  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+  // pass in req.body instead to only update what's passed through
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: "no user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
     })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {
-    User.destroy({
-      where: {
-        id: req.params.id
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
       }
+      res.json(dbUserData);
     })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
